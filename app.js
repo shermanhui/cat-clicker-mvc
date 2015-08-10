303cc668b05bf619059103b7d02849fe4f16e787
shui91 <sherman.sy.hui@gmail.com>
2015-08-09 20:44:04 -0700

    feat: initial commit for catclicker with KO (MVVM instead of MVC)

---
+++ app.js
@@ -0,0 +1,21 @@
+function model = {
+	currentCat: null,
+	cats: [
+	{
+		catName = "one",
+		clickCount = 0,
+		imgSrc = ""
+	}//,
+	// {
+	// 	catName = "two",
+	// 	clickCount = 0,
+	// 	imgSrc = ""
+	// }
+	];
+};
+
+function viewModel = {
+
+};
+
+ko.applyBindings(myViewModel);
